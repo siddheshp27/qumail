868a7eb09f485e47b2db3ff5fff1aad4c00266e5
Aryan Pokharkar <aryan.pokharkar21@vit.edu>
2024-05-23 16:36:34 +0530

    Get server session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,65 +2,68 @@ import { Pool } from 'pg';
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import { auth } from '@/app/session';
 
 export const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {},
-            async authorize(credentials) {
-                const { userName, password } = credentials;
-                console.log(userName,password)
-                try {
-                    const connectionString = process.env.NEON;
-                    const pool = new Pool({
-                        connectionString: connectionString,
-                    });
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const { userName, password } = credentials;
+        console.log(userName, password);
 
-                    const client = await pool.connect();
+        try {
+          const connectionString = process.env.NEON;
+          const pool = new Pool({
+            connectionString: connectionString,
+          });
 
-                    const userQuery = `SELECT PasswordHash FROM "User" WHERE Username = $1`;
-                    const userResult = await client.query(userQuery, [userName]);
-                 
-                    if (userResult.rows.length === 0) {
-                        client.release();
-                        return null;
-                    }
+          const client = await pool.connect();
+          const userQuery = `SELECT PasswordHash FROM "User" WHERE Username = $1`;
+          const userResult = await client.query(userQuery, [userName]);
 
-                    const user = userResult.rows[0].passwordhash;
-                    console.log(user)
-                    // Check if the PasswordHash exists
-                    if (!user) {
-                        client.release();
-                        console.log("Empty password")
-                        return null;
-                    }
+          if (userResult.rows.length === 0) {
+            client.release();
+            return null;
+          }
 
-                    const passwordsMatch = await bcrypt.compare(password, user);
+          const user = userResult.rows[0].passwordhash;
+          console.log(user);
 
-                    client.release();
+          if (!user) {
+            client.release();
+            console.log("Empty password");
+            return null;
+          }
 
-                    if (!passwordsMatch) {
-                        return null;
-                    } else {
-                        console.log("User found");
-                        return { userName };
-                    }
-                } catch (error) {
-                    console.log("Error: ", error);
-                    return null;
-                }
-            },
-        }),
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: "/login",
-    },
+          const passwordsMatch = await bcrypt.compare(password, user);
+          client.release();
+
+          if (!passwordsMatch) {
+            return null;
+          } else {
+            console.log("User found");
+            return { userName };
+          }
+        } catch (error) {
+          console.log("Error: ", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
 };
+
+export const config = authOptions;
+export { auth };
 
 const handler = NextAuth(authOptions);
 
