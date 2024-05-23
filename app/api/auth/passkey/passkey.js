4480e02fce326c78f4591dd1f467539a00ef452a
siddheshp27 <siddheshpatil003@gmail.com>
2024-05-23 16:38:55 +0530

    added passkey login

--- app/api/auth/passkey/passkey.js
+++ app/api/auth/passkey/passkey.js
@@ -1,4 +1,67 @@
-export default function getPassKeyRegister() {
-  console.log();
-  return "hi";
-}
+"use server";
+const challengeStore = {};
+const userStore = {};
+// const {generateRegistrationOptions} =
+import {
+  generateRegistrationOptions,
+  verifyRegistrationResponse,
+} from "@simplewebauthn/server";
+const getRegisterChallenge = async (email) => {
+  //check for email...
+  const url = process.env.URL;
+  console.log(process.env.URL);
+  const challengePayload = await generateRegistrationOptions({
+    rpID: url,
+    rpName: "My Qumail Machine",
+    userName: email,
+  });
+  challengeStore[email] = challengePayload;
+  return { options: challengePayload };
+};
+
+const verifyRegistration = async (req) => {
+  const { email, creds } = req;
+  const url = process.env.URL;
+
+  //chk if email exists
+  const currentOptions = challengeStore[email];
+  const verification = await verifyRegistrationResponse({
+    expectedChallenge: currentOptions.challenge,
+    expectedOrigin: process.env.EXPECTED_ORIGIN,
+    expectedRPID: url,
+    response: creds,
+  });
+
+  if (!verification.verified) return { error: "verification failed" };
+  //add pass key to user
+  const { registrationInfo } = verification;
+  const {
+    credentialID,
+    credentialPublicKey,
+    counter,
+    credentialDeviceType,
+    credentialBackedUp,
+  } = registrationInfo;
+  console.log({
+    user: email,
+    webAuthnUserID: currentOptions.user.id,
+    id: credentialID,
+    publicKey: credentialPublicKey,
+    counter: counter,
+    deviceType: credentialDeviceType,
+    backedUp: credentialBackedUp,
+    transports: creds.response.transports,
+  });
+  return {
+    user: email,
+    webAuthnUserID: currentOptions.user.id,
+    id: credentialID,
+    publicKey: credentialPublicKey,
+    counter: counter,
+    deviceType: credentialDeviceType,
+    backedUp: credentialBackedUp,
+    transports: creds.response.transports,
+  };
+};
+
+export { getRegisterChallenge, verifyRegistration };
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,12 +1,24 @@
+"use client";
 import React from "react";
+import {
+  getRegisterChallenge,
+  verifyRegistration,
+} from "../api/auth/passkey/passkey";
+import { startRegistration } from "@simplewebauthn/browser";
 
 export default function Page() {
   const handleRegisterPassKey = async () => {
-    const response = await getPassKey();
+    const email = "siddheshpatil003@gmail.com";
+    const response = await getRegisterChallenge(email);
+    const { options } = response;
+    const authResult = await startRegistration(options);
+    // console.log(authResult);
+    const temp = await verifyRegistration({ email, creds: authResult });
+    console.log(temp);
   };
   return (
     <div>
-      <h1>Register a Pass key</h1>
+      <h1 onClick={handleRegisterPassKey}>Register a Pass key</h1>
     </div>
   );
 }
